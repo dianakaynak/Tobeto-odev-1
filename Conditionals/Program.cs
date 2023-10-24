dd577211f6147e42971c64028690f925572cc29d
dianakaynak <dianaonder90@gmail.com>
2023-10-24 15:47:57 +0300

    Video 19: Demo: Çoklu Şartlarda Çalışmak

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -2,7 +2,7 @@
 using System.ComponentModel.Design;
 
 Console.WriteLine("Conditionals");
-var number = 11;
+var number = 20;
 //if ( number == 10)
 //{
 //    Console.WriteLine("Number is 10");
@@ -19,18 +19,37 @@ var number = 11;
 //  Console.WriteLine("Number is not 10 or 20");
 //}
 
-switch (number)
-{
-    case 10:
-        Console.WriteLine("Number is 10");
-        break;
-    case 20:
-        Console.WriteLine("Number is 20");
-        break;
-    default:
-        Console.WriteLine(" Number is not 10 or 20 ");
-        break;
+//switch (number)
+//{
+//    case 10:
+//        Console.WriteLine("Number is 10");
+//        break;
+//    case 20:
+//        Console.WriteLine("Number is 20");
+//        break;
+//    default:
+//        Console.WriteLine(" Number is not 10 or 20 ");
+//        break;
 
+
+//}
+
+if (number >= 0 && number <= 100)
+{
+    Console.WriteLine("Number is between 0-100");
 
 }
-Console.ReadLine();
+
+else if (number > 100 && number <= 200)
+{
+
+    Console.WriteLine("Number is between 101-200");
+}
+
+
+else if (number > 200 || number < 0)
+{
+    Console.WriteLine("Number is less than 0 or greater than 200");
+
+}
+    Console.ReadLine();
