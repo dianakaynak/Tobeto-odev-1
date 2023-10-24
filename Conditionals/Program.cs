0268898dc74220c2a2a49f8aa85fdc2f38c7f814
dianakaynak <dianaonder90@gmail.com>
2023-10-24 14:41:46 +0300

    Video 16: Single Line If İle Çalışmak

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -1,13 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine( "Conditionals" );
-var number = 10;
-if (number==10)
-    
-{
-    Console.WriteLine("Number is 10");
-}           
-else
-{
-    Console.WriteLine("Number is not 10"); 
-}
+var number = 11;
+Console.WriteLine( number = 10 ? "Number is 10" : "Number is not 10");
+
 Console.ReadLine();
