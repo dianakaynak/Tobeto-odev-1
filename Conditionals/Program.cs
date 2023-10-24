9efa5e450e48d67fd32651aeccc68dbceb6d99f0
dianakaynak <dianaonder90@gmail.com>
2023-10-24 15:08:25 +0300

    Video 17: Else If Bloğuyla Çalışmak

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -1,6 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.Design;
+
 Console.WriteLine( "Conditionals" );
 var number = 11;
-Console.WriteLine( number = 10 ? "Number is 10" : "Number is not 10");
+if ( number == 10)
+{
+    Console.WriteLine("Number is 10");
 
-Console.ReadLine();
+}
+else if (number == 20);
+{
+    Console.WriteLine("Number is 20");
+
+
+}
+else
+{
+    Console.WriteLine("Number is not 10 or 20");
+}
+    Console.ReadLine();
