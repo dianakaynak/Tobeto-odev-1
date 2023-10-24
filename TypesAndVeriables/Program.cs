c7bff002b2bea555d2e0432030603868394eedf6
dianakaynak <dianaonder90@gmail.com>
2023-10-24 12:37:32 +0300

    Video-5: Integer veri tipi

---
+++ TypesAndVeriables/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//Value Types
+//Console.WriteLine("Hello World!);
+int number1 = 1000000;
+Console.WriteLine("Number1 is {0}", number1);
+Console.ReadLine();
