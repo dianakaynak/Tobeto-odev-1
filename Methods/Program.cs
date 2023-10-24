cfd424204a10aa6bb0ced503d9a2cab9426b19c6
dianakaynak <dianaonder90@gmail.com>
2023-10-25 02:26:54 +0300

    Video 27: Params Keyword İle Çalışmak

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -20,6 +20,11 @@
             Console.WriteLine(Multiply(2,4));
             Console.WriteLine(Multiply(2,4,5));
 
+            Console.WriteLine(Add4(1,2,3,4,5,6));
+
+
+
+
             Console.ReadLine();
         }
 
@@ -50,6 +55,12 @@
             {
                 return number1 * number2 * number3;
             }
+
+        static int Add4(int number,params int[] numbers)
+        {
+            return numbers.Sum();
+        
+        }
         }
     }
 
