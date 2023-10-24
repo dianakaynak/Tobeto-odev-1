f0ea4392f98ea0ee91f362b855449151a8dfca40
dianakaynak <dianaonder90@gmail.com>
2023-10-24 13:28:34 +0300

    Video 8: Byte Veri Tipi

--- TypesAndVeriables/Program.cs
+++ TypesAndVeriables/Program.cs
@@ -1,13 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Types And Veriables");
 //Value Types
 //Console.WriteLine("Hello World!);
+byte number4 = 255;
 short number3 = 32767;
 int number1 = 2147483647;
 long number2 = 9223372036854775807;
 Console.WriteLine("Number1 is {0}", number1);
 Console.WriteLine("Number2 is {0}", number2);
 Console.WriteLine("Number3 is {0}", number3);
+Console.WriteLine("Number4 is {0}", number4);
+
 Console.ReadLine();
 
 
