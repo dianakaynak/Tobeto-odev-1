9aadad4e614ec6983af5c7b964555396baa945bd
dianakaynak <dianaonder90@gmail.com>
2023-10-24 13:49:18 +0300

    Video 11: Double Veri Tipi

--- TypesAndVeriables/Program.cs
+++ TypesAndVeriables/Program.cs
@@ -2,6 +2,7 @@
 Console.WriteLine("Types And Veriables");
 //Value Types
 //Console.WriteLine("Hello World!);
+double number5 = 10.4;
 char character = 'A';
 bool condition = false;
 byte number4 = 255;
@@ -12,6 +13,7 @@ Console.WriteLine("Number1 is {0}", number1);
 Console.WriteLine("Number2 is {0}", number2);
 Console.WriteLine("Number3 is {0}", number3);
 Console.WriteLine("Number4 is {0}", number4);
+Console.WriteLine("Number5 is {0}", number5);
 Console.WriteLine("Character is : {0}",(int) character);
 
 
