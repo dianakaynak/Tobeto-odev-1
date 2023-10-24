4011963a09366bd5693e9eab62c2d1ca9aecfe8c
dianakaynak <dianaonder90@gmail.com>
2023-10-24 15:26:58 +0300

    Video 18: Switch Bloğuyla Çalışmak

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -1,21 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.ComponentModel.Design;
 
-Console.WriteLine( "Conditionals" );
+Console.WriteLine("Conditionals");
 var number = 11;
-if ( number == 10)
+//if ( number == 10)
+//{
+//    Console.WriteLine("Number is 10");
+
+//}
+//else if (number == 20);
+//{
+//  Console.WriteLine("Number is 20");
+
+
+//}
+//else
+//{
+//  Console.WriteLine("Number is not 10 or 20");
+//}
+
+switch (number)
 {
-    Console.WriteLine("Number is 10");
+    case 10:
+        Console.WriteLine("Number is 10");
+        break;
+    case 20:
+        Console.WriteLine("Number is 20");
+        break;
+    default:
+        Console.WriteLine(" Number is not 10 or 20 ");
+        break;
+
 
 }
-else if (number == 20);
-{
-    Console.WriteLine("Number is 20");
-
-
-}
-else
-{
-    Console.WriteLine("Number is not 10 or 20");
-}
-    Console.ReadLine();
+Console.ReadLine();
