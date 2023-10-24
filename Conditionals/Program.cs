d87e95f718bd85e02b46faaa63bd4dfffee38f93
dianakaynak <dianaonder90@gmail.com>
2023-10-24 14:33:19 +0300

    Video 15: If Bloklarıyla Çalışmaya Başlamak

---
+++ Conditionals/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine( "Conditionals" );
+var number = 10;
+if (number==10)
+    
+{
+    Console.WriteLine("Number is 10");
+}           
+else
+{
+    Console.WriteLine("Number is not 10"); 
+}
+Console.ReadLine();
