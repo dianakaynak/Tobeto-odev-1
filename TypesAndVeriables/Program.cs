d56ab551ffa6cd0af0a8e6e65179e453c82b390a
dianakaynak <dianaonder90@gmail.com>
2023-10-24 12:47:10 +0300

    Video 6: Long Veri Tipi

--- TypesAndVeriables/Program.cs
+++ TypesAndVeriables/Program.cs
@@ -2,6 +2,11 @@
 Console.WriteLine("Hello, World!");
 //Value Types
 //Console.WriteLine("Hello World!);
-int number1 = 1000000;
+int number1 = 2147483647;
+long number2 = 9223372036854775807;
 Console.WriteLine("Number1 is {0}", number1);
+Console.WriteLine("Number2 is {0}", number2);
 Console.ReadLine();
+
+
+
