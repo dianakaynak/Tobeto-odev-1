77987a7f533b14c0976de8b6a82a0049969cfbe0
dianakaynak <dianaonder90@gmail.com>
2023-10-25 10:57:00 +0300

    Video 31: While Döngüsü

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -4,12 +4,28 @@
     {
         static void Main(string[] args)
         {
-            for (int i = 100; i >=0; i=i-2)
+            //ForLoop();
+            int number = 100;
+            while (number >=0) 
             
-            {
-               Console.WriteLine(i);
+            { 
+               Console.WriteLine(number);
+
+                number--;   
+            
             }
+        }
+
+        private static void ForLoop()
+        {
+            for (int i = 100; i >= 0; i = i - 2)
+
+            {
+                Console.WriteLine(i);
+            }
+
             Console.WriteLine("Finished!");
+
         }
     }
 }
