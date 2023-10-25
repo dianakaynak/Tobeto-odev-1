b26beb0fa563fd347a76c165afcf370a64a360da
dianakaynak <dianaonder90@gmail.com>
2023-10-26 00:31:44 +0300

    Video 39 : Encapsulation

--- Classes/Customer.cs
+++ Classes/Customer.cs
@@ -7,10 +7,22 @@ using System.Threading.Tasks;
 namespace Classes
 {
     internal class Customer
-    {
-        public string Firstname;
+    {     
+        
+       
+       //Property - özellik tanımlamak
         public int Id { get; set; }
-        //public string FirstName { get; set; }
+
+        private string _firstName; 
+        public string FirstName
+        {
+            get
+            { return "Mrs." + _firstName; }
+
+            set
+
+            {_firstName = value;}
+        }
         public string LastName { get; set; }
         public string City { get; set; }
 
